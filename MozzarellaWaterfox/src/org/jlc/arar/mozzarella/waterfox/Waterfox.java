0c3f82c1ff065891c9806803c880b3156e0fc938
LeaChemoul <34442368+LeaChemoul@users.noreply.github.com>
2018-05-24 09:29:59 +0200

    Tentative Client

--- MozzarellaWaterfox/src/org/jlc/arar/mozzarella/waterfox/Waterfox.java
+++ MozzarellaWaterfox/src/org/jlc/arar/mozzarella/waterfox/Waterfox.java
@@ -9,6 +9,10 @@ import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
 public class Waterfox extends Application {
 	
 	private BorderPane root;
@@ -33,5 +37,78 @@ public class Waterfox extends Application {
 		bt_refresh = new Button("Refresh");
 		w_view = new WebView();
 		w_engine = w_view.getEngine();
+
+		try {
+			System.out.println("Tentative de connexion");
+			Socket con_serv = new Socket(InetAddress.getByAddress("127.0.0.1".getBytes()),80);
+			System.out.println("Connexion etablie");
+
+			//flux de sortie
+			InputStream inp = con_serv.getInputStream();
+
+			//flux d'entrée
+			OutputStream op = con_serv.getOutputStream();
+
+
+			BufferedReader in = new BufferedReader(new InputStreamReader(inp));
+			PrintWriter printWriter = new PrintWriter(op);
+
+			Scanner sc = new Scanner(System.in);
+
+			Thread envoyer = new Thread(new Runnable() {
+				String message;
+				@Override
+				public void run() {
+					while(true){
+						message = sc.nextLine();
+						printWriter.println(message);
+						printWriter.flush();
+					}
+				}
+			});
+			envoyer.start();
+
+			Thread recevoir = new Thread(new Runnable() {
+				String message;
+				@Override
+				public void run() {
+					try {
+						message = in.readLine();
+						while(message!=null){
+							System.out.println("Serveur : " + message);
+							message = in.readLine();
+						}
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+					System.out.println("Serveur déconecté");
+					printWriter.close();
+					try {
+						con_serv.close();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			});
+
+			recevoir.start();
+
+			/*String response = in.readLine();
+			if (response.contains("ok")) {
+				System.out.println("Connexion établie");
+				System.out.println(response);
+			} else {
+				while (true) {
+					System.out.print("La connexion n'a pas pu être établie.");
+				}
+			}*/
+
+			con_serv.close();
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
+
 	}
 }
